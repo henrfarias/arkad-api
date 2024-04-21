90cf65e9b0616bf4bde97fea24dfa5d9a76879cf
Henrique Farias <henrique.biasibetti@gmail.com>
2024-04-21 17:37:07 -0300

    refactor: enrich treasure paper entity interface

---
+++ src/domain/interfaces/entity/treasure_paper.ts
@@ -0,0 +1,29 @@
+export interface RawTreasure {
+  'Tipo Titulo': string
+  'Data Vencimento': string
+  'Data Base': string
+  'Taxa Compra Manha': string
+  'Taxa Venda Manha': string
+  'PU Compra Manha': string
+  'PU Venda Manha': string
+  'PU Base Manha': string
+}
+
+export enum Indexes {
+  SELIC = 'selic',
+  POUP = 'poup'
+}
+
+export interface TreasurePaperEntity {
+  title: string
+  refDate: Date
+  dueDate: Date
+  index: Indexes
+  purchaseFee: number
+  purchasePrice: number
+  salePrice: number
+}
+
+export interface TreasurePaper extends TreasurePaperEntity {
+  id?: string
+}
