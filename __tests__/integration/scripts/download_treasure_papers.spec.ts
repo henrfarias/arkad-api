ccaa4d3c0e28c769e6edeeb3d347cd5650675335
Henrique Farias <henrique.biasibetti@gmail.com>
2024-04-27 09:40:39 -0300

    feat: setup lambda function to run download and persistance

--- __tests__/integration/scripts/download_treasure_papers.spec.ts
+++ __tests__/integration/scripts/download_treasure_papers.spec.ts
@@ -1,13 +1,18 @@
+import { csvStream } from '@framework/libs/fast-csv'
+import { PrismaTreasurePaperRepository } from '@framework/repositories/prisma_treasure_paper.repository'
 import { BacenTreasurePaper } from 'src/scripts/seed_treasure_paper/bacen_treasure_paper'
-import { getBrazilianTreasurePapers } from 'src/scripts/seed_treasure_paper/get_treasure_papers_from_file'
+import { GetTreasurePapersFromStream } from 'src/scripts/seed_treasure_paper/get_treasure_papers_from_stream'
 import { describe, test } from 'vitest'
 
 describe('download_treasure_papers', () => {
   test('should download the file from the website passing through the pipeline', async () => {
-    const treasureHandler = new BacenTreasurePaper('Tesouro Selic')
-    await getBrazilianTreasurePapers(
-      treasureHandler,
-      await treasureHandler.download()
+    const repository = new PrismaTreasurePaperRepository()
+    const treasureHandler = new BacenTreasurePaper('Tesouro Selic', repository)
+    const getBrazilianTreasurePapers = new GetTreasurePapersFromStream(
+      await treasureHandler.download(),
+      csvStream,
+      treasureHandler
     )
+    await getBrazilianTreasurePapers.execute()
   }, 60000)
 })
---
+++ src/framework/serverless/seedTreasure.ts
@@ -0,0 +1,18 @@
+import { csvStream } from '@framework/libs/fast-csv'
+import { PrismaTreasurePaperRepository } from '@framework/repositories/prisma_treasure_paper.repository'
+import { BacenTreasurePaper } from 'src/scripts/seed_treasure_paper/bacen_treasure_paper'
+import { GetTreasurePapersFromStream } from 'src/scripts/seed_treasure_paper/get_treasure_papers_from_stream'
+
+export async function seedTreasure(): Promise<void> {
+  const repository = new PrismaTreasurePaperRepository()
+  const handler = new BacenTreasurePaper('Tesouro Selic', repository)
+  const stream = await handler.download()
+  const csvToJson = csvStream
+  const getTreasures = new GetTreasurePapersFromStream(
+    stream,
+    csvToJson,
+    handler
+  )
+  await getTreasures.execute()
+  return
+}
